2d470b89d65132f7c360486d9ad6d4d13e7fc79e
michael-zanaglia <michael.zanaglia@laplateforme.io>
2024-09-22 20:22:34 +0200

    Header Fixed

--- frontend/src/Index.jsx
+++ frontend/src/Index.jsx
@@ -1,12 +1,13 @@
-import "./styles/header.css"
-import "./styles/footer.css"
+import "./styles/style.css"
 import "./styles/index.css"
-
+import Header from "./components/useAgain/Header/Header"
+import Footer from "./components/useAgain/Footer/Footer"
 
 export default function Index(){
     return(
         <>
-        
+            <Header theme={'#559D53'} color={'#F7F5F5'} hoverTheme={'#448C42'}/>
+            <Footer/>
         </>
     )
 }
