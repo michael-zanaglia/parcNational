d58edd18b91ab25f98d4c2738fb4063c34d70cca
michael-zanaglia <michael.zanaglia@laplateforme.io>
2024-09-23 16:50:39 +0200

    Add : Index Page Maquettage

--- frontend/src/Index.jsx
+++ frontend/src/Index.jsx
@@ -2,12 +2,12 @@ import "./styles/style.css"
 import "./styles/index.css"
 import Header from "./components/useAgain/Header/Header"
 import Footer from "./components/useAgain/Footer/Footer"
+import MainIndex from "./components/MainIndex/MainIndex"
 
 export default function Index(){
     return(
         <>
-            <Header theme={'#559D53'} color={'#F7F5F5'} hoverTheme={'#448C42'}/>
-            <Footer/>
+           <MainIndex/>
         </>
     )
 }
---
+++ frontend/src/components/MainIndex/MainIndex.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import Footer from "../useAgain/Footer/Footer";
+import Header from "../useAgain/Header/Header";
+import IndexCard from "./IndexCard/IndexCard";
+
+export default function MainIndex(){
+    const [blocked, setBlocked] = useState(false);
+    useEffect(()=>{
+        if(blocked){
+            document.body.style.overflow = 'hidden';
+            document.body.style.height = '100vh';
+        } else {
+            document.body.style.overflow = 'visible';
+            document.body.style.height = 'auto';
+        }
+    },[blocked])
+    return(
+        <main>
+            <div className="indexItems first">
+                <Header theme={'#559D53'} color={'#F7F5F5'} hoverTheme={'#448C42'} setBlocked={setBlocked}/>
+                <div className="image"><p>img</p></div>
+            </div>
+
+            <div className="indexItems two">
+                <h1>Découvrir le Parc du Mercantour</h1>  
+                <div className="cards">
+                    {Array.from({ length: 3 }, (_, i) => (
+                        <IndexCard key={i} />
+                    ))}
+                </div>   
+            </div>
+
+            <div className="indexItems three">
+                <h1>Nous trouver</h1>
+                <div className="map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2862.0911638686057!2d7.06283777586768!3d44.163980519246174!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12cdaf6678af879d%3A0xcabc15aee7b89386!2sParc%20national%20du%20Mercantour!5e0!3m2!1sfr!2sfr!4v1727092323248!5m2!1sfr!2sfr"  height="450" style={{border: 0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+                <Footer/>
+            </div>
+        </main>
+    )
+}
