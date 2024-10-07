1555214e8dce89b2c407a0c27eea4e144bf2a6e9
Rhubarb06150 <matheogass@outlook.com>
2024-10-07 10:35:15 +0200

    API pour login et signup

--- launch.sh
+++ launch.sh
@@ -4,5 +4,3 @@ sudo /opt/lampp/lampp stop
 sudo service docker start
 sudo systemctl start docker
 sudo docker compose up --build&
-firefox http://localhost:8081 > /dev/null&
-firefox http://localhost:8080 > /dev/null
